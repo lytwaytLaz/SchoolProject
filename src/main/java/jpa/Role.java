4e7cecb7073e72715ab8863c21b01c6a3e4ea36c
lytwaytLaz <laszlo.hago@gmail.com>
2016-09-07 16:05:12 +0200

    "add changes"

---
+++ src/main/java/jpa/Role.java
@@ -0,0 +1,41 @@
+package jpa;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * @author László Hágó
+ * @version 1.0
+ * @since 2016-09-07
+ */
+@Entity
+public class Role
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private String id;
+    private String role;
+
+
+    public String getId()
+    {
+        return id;
+    }
+
+    public void setId(String id)
+    {
+        this.id = id;
+    }
+
+    public String getRole()
+    {
+        return role;
+    }
+
+    public void setRole(String role)
+    {
+        this.role = role;
+    }
+}
