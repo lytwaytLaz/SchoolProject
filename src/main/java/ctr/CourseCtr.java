37361d72b96681d121b7f4d6f6d79833cdfea61f
lytwaytLaz <laszlo.hago@gmail.com>
2016-09-12 08:10:39 +0200

    "change variable names"

--- src/main/java/ctr/CourseCtr.java
+++ src/main/java/ctr/CourseCtr.java
@@ -5,24 +5,24 @@ package ctr;
  */
 public class CourseCtr {
 
-    private Long id;
+    private Long course_id;
     private String courseCtrName;
 
     public CourseCtr(String courseCtrName) {
         this.courseCtrName = courseCtrName;
     }
 
-    public CourseCtr(Long id, String courseCtrName) {
-        this.id = id;
+    public CourseCtr(Long course_id, String courseCtrName) {
+        this.course_id = course_id;
         this.courseCtrName = courseCtrName;
     }
 
-    public Long getId() {
-        return id;
+    public Long getCourse_id() {
+        return course_id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public void setCourse_id(Long course_id) {
+        this.course_id = course_id;
     }
 
     public String getCourseCtrName() {
